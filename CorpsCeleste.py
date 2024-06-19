15a5540d8e5354bcc7da68588ac3fa0cd06ffdfc
Thomas Lyautey <tlyautey1@gmail.com>
2024-06-19 09:55:12 +0200

    Feat : Initialisation Classe CorpsCeleste et création de son constructeur

---
+++ CorpsCeleste.py
@@ -0,0 +1,8 @@
+class CorpsCeleste:
+    def __init__(self, nom, x, y, rayon, masse, couleur):
+        self.nom = nom
+        self.x = x
+        self.y = y
+        self.rayon = rayon
+        self.masse = masse
+        self.couleur = couleur

8791fe4371a919a8fe3eb2289f7229ea4b2a536f
Thomas Lyautey <tlyautey1@gmail.com>
2024-06-19 10:12:36 +0200

    Feat : Ajout des variables dans la classe CorpsCeleste : Constante Gravitationnelle, Unité Astronomique, Echelle (250 px par UA) et une journée en secondes

--- CorpsCeleste.py
+++ CorpsCeleste.py
@@ -1,4 +1,9 @@
 class CorpsCeleste:
+    G = 6.67430e-11  # Constante gravitationnelle
+    AU = 1.496e11  # Unité astronomique en mètres
+    SCALE = 250 / AU  # Échelle de l'affichage (250 pixels par unité astronomique)
+    TIMESTEP = 3600 * 24  # Une journée en secondes
+    
     def __init__(self, nom, x, y, rayon, masse, couleur):
         self.nom = nom
         self.x = x
@@ -6,3 +11,4 @@ class CorpsCeleste:
         self.rayon = rayon
         self.masse = masse
         self.couleur = couleur
+

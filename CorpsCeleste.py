a205a2b593eaf05d8a4b93c3be4bfbf9cc0bf018
Thomas Lyautey <tlyautey1@gmail.com>
2024-06-19 10:21:06 +0200

    Feat : Ajout des fonctions distanceAvecAutreCorpsCeleste(self, autreCorpsCeleste) qui retourne la distance entre 2 corps en m et forceGravitationnelle(self, autreCorpsCeleste) qui calcule la force gravitationnelle entre deux Corps

--- CorpsCeleste.py
+++ CorpsCeleste.py
@@ -1,7 +1,9 @@
+from math import sqrt
+
 class CorpsCeleste:
     G = 6.67430e-11  # Constante gravitationnelle
-    AU = 1.496e11  # Unité astronomique en mètres
-    SCALE = 250 / AU  # Échelle de l'affichage (250 pixels par unité astronomique)
+    UA = 1.496e11  # Unité astronomique en mètres (Distance Terre-Soleil)
+    SCALE = 250 / UA  # Échelle de l'affichage (250 pixels par unité astronomique)
     TIMESTEP = 3600 * 24  # Une journée en secondes
     
     def __init__(self, nom, x, y, rayon, masse, couleur):
@@ -12,3 +14,8 @@ class CorpsCeleste:
         self.masse = masse
         self.couleur = couleur
 
+    def distanceAvecAutreCorpsCeleste(self, autreCorpsCeleste):
+        return sqrt((self.x - autreCorpsCeleste.x)**2, (self.y - autreCorpsCeleste.y)**2) / self.SCALE
+
+    def forceGravitationnel(self, autreCorpsCeleste):
+        return self.G * self.masse * autreCorpsCeleste.masse / self.distanceAvecAutreCorpsCeleste(self, autreCorpsCeleste)**2

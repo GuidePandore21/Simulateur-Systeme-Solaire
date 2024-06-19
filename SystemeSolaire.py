cc9318d6988a96ebabeb0f556c979346cd998a3d
Thomas Lyautey <tlyautey1@gmail.com>
2024-06-19 15:46:05 +0200

    Feat : Création de la classe SystemeSolaire avec son constructeur et une fonction ajouterCorpsCeleste(self, corpsCeleste)

---
+++ SystemeSolaire.py
@@ -0,0 +1,6 @@
+class SystemeSolaire:
+    def __init__(self):
+        self.listeCorpsCeleste = []
+    
+    def ajouterCorpsCeleste(self, corpsCeleste):
+        self.listeCorpsCeleste.append(corpsCeleste)

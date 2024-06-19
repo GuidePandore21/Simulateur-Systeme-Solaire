ebad2d0b578e199fa8c12b3baa9c9c72a9ffbaaa
Thomas Lyautey <tlyautey1@gmail.com>
2024-06-19 11:31:11 +0200

    Feat : Création fonction calculPositionEtVitesse(self, autresCorpsCelestes) dans la Classe CorpsCeleste

--- CorpsCeleste.py
+++ CorpsCeleste.py
@@ -25,3 +25,25 @@ class CorpsCeleste:
         forceX = force * (autreCorpsCeleste.x - self.x) / self.distance
         forceY = force * (autreCorpsCeleste.y - self.y) / self.distance
         return forceX, forceY
+    
+    def calculPositionEtVitesse(self, autresCorpsCelestes):
+        totaleForceX = 0
+        totaleForceY = 0
+        
+        for autreCorpsCeleste in autresCorpsCelestes:
+            if autreCorpsCeleste != self:
+                forceX, forceY = self.forceGravitationnelle(self, autreCorpsCeleste)
+                totaleForceX += forceX
+                totaleForceY += forceY
+        
+        # Calcul des accélérations : m/s²
+        accelerationX = totaleForceX / self.masse
+        accelerationY = totaleForceY / self.masse
+        
+        # Mise à jour des vitesses : m/s
+        self.vitesseX += accelerationX * self.TIMESTEP
+        self.vitesseY += accelerationY * self.TIMESTEP
+        
+        # Mise à jour des positions : px
+        self.x += self.vitesseX * self.TIMESTEP * self.SCALE
+        self.y += self.vitesseY * self.TIMESTEP * self.SCALE

6222cbfd2912c3ea8e2bb08e6f580c585be70ec9
Thomas Lyautey <tlyautey1@gmail.com>
2024-06-19 10:53:16 +0200

    Refactor : Modification du constructeur de la Classe CorpsCeleste pour ajouter les variables vitesseX et vitesseY

--- CorpsCeleste.py
+++ CorpsCeleste.py
@@ -6,10 +6,12 @@ class CorpsCeleste:
     SCALE = 250 / UA  # Échelle de l'affichage (250 pixels par unité astronomique)
     TIMESTEP = 3600 * 24  # Une journée en secondes
     
-    def __init__(self, nom, x, y, rayon, masse, couleur):
+    def __init__(self, nom, x, y, vitesseX, vitesseY, rayon, masse, couleur):
         self.nom = nom
         self.x = x
         self.y = y
+        self.vitesseX = vitesseX
+        self.vitesseY = vitesseY
         self.rayon = rayon
         self.masse = masse
         self.couleur = couleur
@@ -20,6 +22,6 @@ class CorpsCeleste:
     def forceGravitationnelle(self, autreCorpsCeleste):
         distance = self.distanceAvecAutreCorpsCeleste(self, autreCorpsCeleste)
         force = self.G * self.masse * autreCorpsCeleste.masse / distance**2
-        forceX = force * (autreCorpsCeleste.x - self.x) / self.SCALE
-        forceY = force * (autreCorpsCeleste.y - self.y) / self.SCALE
+        forceX = force * (autreCorpsCeleste.x - self.x) / self.distance
+        forceY = force * (autreCorpsCeleste.y - self.y) / self.distance
         return forceX, forceY

90553b823cacfed8f07c17d4420d3a370289c5a2
Thomas Lyautey <tlyautey1@gmail.com>
2024-06-24 14:45:27 +0200

    Feat : Initialisation du Soleil et de la Terre dans main.py

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import pygame
+from SystemeSolaire import SystemeSolaire
+from CorpsCeleste import CorpsCeleste
 
 WIDTH = 500
 HEIGHT = 500
+
+jaune = (255, 255, 0)
+bleu = (0, 0, 255)
+
+systemeSolaire = SystemeSolaire()
+
+Soleil = CorpsCeleste("Soleil", WIDTH//2, HEIGHT//2, 0, 0, 696340000, 1.989e30, jaune)
+systemeSolaire.ajouterCorpsCeleste(Soleil)
+
+Terre = CorpsCeleste("Terre", WIDTH//2 + CorpsCeleste.UA * CorpsCeleste.SCALE, HEIGHT//2, 0, 0, 6371000, 5.972e24)
+systemeSolaire.ajouterCorpsCeleste(Terre)
 
 pygame.init()
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
